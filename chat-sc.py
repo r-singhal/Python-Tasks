a67cadade43e18d49b38f71d641d03d7e93792af
Rishabh Singhal <35341344+r-singhal@users.noreply.github.com>
2018-05-26 12:46:24 +0530

    one 2 one chat with name just like whatsapp

---
+++ chat-sc.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import socket 
+import thread
+
+i=1
+
+rec_ip="127.0.0.1"
+myport=8888
+s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind((rec_ip, myport))
+
+x=raw_input("Enter your name: ")
+s.sendto(x,("192.168.10.137",8888))
+d=s.recvfrom(100)
+
+def rc():
+	while True:
+		y=s.recvfrom(1000)
+		print (d[0]+":"+y[0])
+
+def sc():
+	while True:
+		a=raw_input()
+		s.sendto(a,("192.168.10.137",8888))
+
+thread.start_new_thread(rc,())
+
+thread.start_new_thread(sc,())
+
+while True:
+	pass
